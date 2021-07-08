2a2f6bf224ff4aa778690f9f9da2f2c2b452f8c0
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-07-08 09:48:26 +0530

    assignment-3

---
+++ block-BNaacq/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, lowercase: true },
+    age: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
